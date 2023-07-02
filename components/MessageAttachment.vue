65199c77fddb30a7913035b0170950a5a8e9a251
William <will27528@gmail.com>
2023-07-02 20:54:43 +0100

    Fixup text attachment previews

--- components/MessageAttachment.vue
+++ components/MessageAttachment.vue
@@ -1,32 +1,28 @@
 <template>
     <div class="attachment">
-        <img class="image-attachment shadow" v-if="type === 'image'" :src="attachment.url"
+        <img class="image-attachment shadow" v-if="getType() === 'Image'" :src="attachment.url"
             @error="this.target.src = '/images/missing-image.png'" alt="Message attachment" />
-        <video class="video-attachment shadow" v-else-if="type === 'video'" controls :src="attachment.url"
+        <video class="video-attachment shadow" v-else-if="getType() === 'Video'" controls :src="attachment.url"
             alt="Message attachment" />
-        <div class="audio-attachment boxed-attachment shadow" v-else-if="type === 'audio'">
+        <div class="audio-attachment boxed-attachment shadow" v-else-if="getType() === 'Audio'">
             <a class="attachment-link" :href="attachment.url" target="_blank" rel="noopener noreferrer">
                 <IconifiedText icon="fa6-solid:file-audio">{{ attachment.name }}</IconifiedText>
             </a>
-            <span>Audio file</span>
             <audio controls :src="attachment.url" alt="Message attachment" />
         </div>
-        <div class="text-attachment boxed-attachment shadow" v-else-if="type === 'text'">
-            <article class="preview">
-                <code>
-                    <pre> {{ getText() }} </pre>
-                </code>
-            </article>
+        <div class="text-attachment boxed-attachment shadow" v-else-if="getType() === 'Text'">
             <a class="attachment-link" :href="attachment.url" target="_blank" rel="noopener noreferrer">
                 <IconifiedText icon="fa6-solid:file-arrow-down">{{ attachment.name }}</IconifiedText>
             </a>
-            <span>Text file</span>
+            <code class="preview">
+                <pre> {{ text }} </pre>
+            </code>
         </div>
         <div class="file-attachment boxed-attachment shadow" v-else>
             <a class="attachment-link" :href="attachment.url" target="_blank" rel="noopener noreferrer">
                 <IconifiedText icon="fa6-solid:file-arrow-down">{{ attachment.name }}</IconifiedText>
             </a>
-            <span>File attachment</span>
+            <span>{{ getType() }} attachment</span>
         </div>
     </div>
 </template>
@@ -39,6 +35,15 @@
     max-width: 50vw;
     margin: 0.5rem 0;
     border-radius: 0.5rem;
+}
+
+.attachment .preview {
+    max-height: 45vh;
+    max-width: 50vw;
+    margin: 0.5rem 0;
+    padding-left: 1rem;
+    border-radius: 0.5rem;
+    overflow: scroll;
 }
 
 .boxed-attachment {
@@ -62,29 +67,32 @@ const { attachment } = defineProps({
     }
 });
 
-const type = () => {
+const getType = () => {
     if (attachment.type.startsWith('image')) {
-        return 'image';
+        return 'Image';
     } else if (attachment.type.startsWith('video')) {
-        return 'video';
+        return 'Video';
     } else if (attachment.type.startsWith('audio')) {
-        return 'audio';
+        return 'Audio';
     } else if (attachment.type.startsWith('text')) {
-        return 'text';
+        return 'Text';
     } else {
-        return 'file';
+        return 'File';
     }
 }
 
-const getText = () => {
-    return fetch(attachment.url)
+const text = getType() === 'Text'
+    ? await fetch(attachment.url, new Headers({
+        'Accept': 'text/plain',
+        'Content-Type': 'text/plain'
+    }))
         .then(response => {
-            if (response.ok) {
+            if (response && response.ok) {
                 return response.text();
             } else {
-                return attachment.name;
+                return "Download to view file";
             }
         })
-        .catch(() => attachment.name);
-}
+        .catch(() => "Download to view file")
+    : attachment.name;
 </script>
